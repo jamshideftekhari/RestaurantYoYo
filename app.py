28d0ee299fd84d5b761d133681eb6a1a176a63dd
jamshideftekhari <jam.eftekhari@gmail.com>
2023-03-29 23:29:20 +0200

    version 3 with post

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for
+from mySqlConnector import MySqlConnector
 
 app = Flask(__name__)
+
+localHost = "localhost"
+user = "root"
+pw = "jam2003eft" 
+db = "resturantyoyosimple"                   
+dbconnection=MySqlConnector(localHost, user, pw, db)
+dbconnection.connect()
+query = "SELECT * FROM resturantyoyosimple.resturant"
 
 @app.route('/')
 def Welcome():
@@ -36,19 +45,41 @@ def resturant():
     resturants2 = [('yoyo', 'adress', 'phone number', 'Email'),
                    ('yoyo2', 'adress2', 'phone number 2', 'Email 2'), 
                    ('yoyo3', 'adress3', 'phone number 3', 'Email 3')]
-    return render_template('resturant.html', r1=resturants1, r2=resturants2)
+    resturants = dbconnection.queryDB(query)
+    return render_template('resturant.html', r1=resturants1, r2=resturants2, r3=resturants)
 
 @app.route('/resturantBS')
 def resturantBS():
     resturants1 = ['yoyo', 'yoyo2', 'yoyo3']
     resturants2 = [('yoyo', 'adress', 'phone number', 'Email'),
                    ('yoyo2', 'adress2', 'phone number 2', 'Email 2'), 
-                   ('yoyo3', 'adress3', 'phone number 3', 'Email 3')]
-    return render_template('resturantBS.html', r1=resturants1, r2=resturants2)
+                   ('yoyo3', 'adress3', 'phone number 3', 'Email 3'),
+                   ('yoyo4', 'adress4', 'phone number 4', 'Email 4')]
+    resturants = dbconnection.queryDB(query)
+    return render_template('resturantBS.html', r1=resturants1, r2=resturants2, r3=resturants)
 
 @app.route('/deliveryOrder')
 def deliveryOrder():
     return render_template('deliveryOrder.html')
+
+# post methods
+@app.route('/createResturant')
+def createResturant():
+    return render_template('createResturant.html')
+
+@app.route('/addResturant', methods=['POST'])
+def addResturant():
+    print(request.form['rname'], request.form['adress'], request.form['phone'], request.form['email'])
+    name = request.form['rname']
+    adress = request.form['adress']
+    phone = request.form['phone']
+    email = request.form['email']
+    query = "INSERT INTO resturantyoyosimple.resturant (ResturantName, Address, Tlf, Email) VALUES ('{}', '{}', '{}', '{}')".format(name, adress, phone, email)
+    #dbconnection.connect()
+    dbconnection.queryDB(query)
+    return redirect(url_for('resturantBS'))
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)                
